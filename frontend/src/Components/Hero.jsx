89a2c5e41ff02e394a63a60209c93a2f8f72f0cc
Vinicius Bernardo <25652392+bgvinicius@users.noreply.github.com>
2020-05-19 22:38:31 -0300

    corrigir memory leak da movimentacao

--- frontend/src/Components/Hero.jsx
+++ frontend/src/Components/Hero.jsx
@@ -1,31 +1,38 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import hero from '../assets/hero.png'
 export default function Hero () {
 
     const [posx, setPosx] = useState(0)
     const [posy, setPosy] = useState(0)
-    window.addEventListener("keydown", KeyDown)
-    function KeyDown(event) {
-        let varx = posx
-        let vary = posy
-        console.log(varx, vary)
-        switch (event.keyCode) {
-            case 37://LEFT
-                setPosx(posx - 50)
-                break
-            case 38://UP
-                setPosy(posy - 50)
-                break
-            case 39://RIGHT
-                setPosx(posx + 50)
-                break
-            case 40://DOWN
-                setPosy(posy + 50)
-                break
-            default:
-                break
+
+    useEffect(() => {
+        window.addEventListener("keydown", KeyDown)
+        function KeyDown(event) {
+            let varx = posx
+            let vary = posy
+            console.log(varx, vary)
+            switch (event.keyCode) {
+                case 37://LEFT
+                    setPosx(posx - 50)
+                    break
+                case 38://UP
+                    setPosy(posy - 50)
+                    break
+                case 39://RIGHT
+                    setPosx(posx + 50)
+                    break
+                case 40://DOWN
+                    setPosy(posy + 50)
+                    break
+                default:
+                    break
+            }
         }
-    }
+
+        return () => {
+            window.removeEventListener("keydown", KeyDown)
+          }
+    }, [posx, posy])
    
     const style = {
         top: posy,
